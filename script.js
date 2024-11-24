2f2558d82568b6928bc636d7721e45eba820cff9
Arkadi Statsenko <arkadistatsenko@gmail.com>
2024-11-24 17:44:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function aaaa() {
+    if (true) {
+        var a = 1
+        let b = "jifwoifj fj jfuoiefjoöeusrh"
+
+        //b = 4
+        console.log(b)
+    }
+    var a = 3
+    console.log(a)
+
+    let f = 1 === "1"
+    let h = 2
+    if (h >= 1) {
+        console.log("true!")
+    } else {
+        console.log("false!")
+    }
+
+    function mult(a, b) {
+        return a * b
+    }
+
+    c = mult(3, 4)
+    console.log(c)
+}
+function season(month){
+    if (month >= 1 && month <= 2 || month == 12){
+        return "winter"
+    }
+
+    if (month >=3 && month <=5){
+        return "spring"
+    }
+
+    if (month >=6 && month <=8){
+        return "summer"
+    }
+
+    if (month >=9 && month <=11){
+        return "autumn"
+    }
+
+    return "Invalid month number. Should be in range 1-12"
+}
+
+let a = [1,2,3,4]
+let b = 1
+function func(a, b){
+    a[0]=0
+    b = 0
+}
+
+
+
+func(a,b)
+
+if ()
+
