0e0c05e3878f4f07322b14c4407afbae8ba0724b
Arkadi Statsenko <arkadistatsenko@gmail.com>
2025-01-12 16:56:04 +0200

    added stuff

--- saturday-15-30/random_lesson.py
+++ saturday-15-30/random_lesson.py
@@ -4,17 +4,34 @@ import os
 import base64
 
 
-#random_bytes = os.urandom(32)
-#print(base64.b64encode(random_bytes).decode("utf-8"))
+random_bytes = os.urandom(32)
+print(base64.b64encode(random_bytes).decode("utf-8"))
 
-random_number = random.randrange(100)
+def guess_a_number():
+    random_number = random.randrange(100)
 
-for i in range(7,1,-1):
-    user_number = int(input("Enter number (You have {} lives left): ".format(i)))
-    if (user_number > random_number):
-        print("Your number is too big")
-    elif (user_number < random_number):
-        print("Your number is too small")
-    if (user_number == random_number):
+    for i in range(7,1,-1):
+        user_number = int(input("Enter number (You have {} lives left): ".format(i)))
+        if (user_number > random_number):
+            print("Your number is too big")
+        elif (user_number < random_number):
+            print("Your number is too small")
+        if (user_number == random_number):
+            print("You win!")
+            break
+
+def rock_paper_scissors():
+    computer_choice = random.choice(["камень", "ножницы", "бумага"])
+    user_choice = input("Enter value (камень, ножницы, бумага): ")
+
+    print("Computer choice: " + computer_choice)
+
+    if (user_choice == "камень" and computer_choice == "ножницы") or \
+         (user_choice == "ножницы" and computer_choice == "бумага"):
         print("You win!")
-        break
+    elif (user_choice == computer_choice):
+        print("Draw")
+    else:
+        print("Computer wins")
+
+#rock_paper_scissors()
