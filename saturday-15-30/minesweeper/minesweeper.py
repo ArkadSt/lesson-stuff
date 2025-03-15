77cf435d7007553b714ee6d1debb07145b83dbe1
Arkadi Statsenko <arkadistatsenko@gmail.com>
2025-03-15 17:49:14 +0200

    finished minesweeper almost (saturday)

--- saturday-15-30/minesweeper/minesweeper.py
+++ saturday-15-30/minesweeper/minesweeper.py
@@ -6,20 +6,29 @@ class Cell:
         self._has_mine = False
         self._neighbouring_mines = 0
         self._display = '-'
+
     def has_mine(self):
         return self._has_mine
+    
     def set_mine(self):
         self._has_mine = True
+
     def open(self):
         self._opened = True
         self._display = ' '
+
     def is_opened(self):
         return self._opened
+    
     def increment_neighbouring_mines(self):
         self._neighbouring_mines += 1
         self._display = str(self._neighbouring_mines)
+        
     def __str__(self):
         return self._display
+    
+    def get_neighbouring_mines_number(self):
+        return self._neighbouring_mines
 
 
 def display_board(board):
@@ -31,10 +40,10 @@ def display_board(board):
     for y, row in enumerate(board):
         print(y, end = " ")
         for cell in row:
-            if cell.has_mine():
-                print("*", end = " ")
-            else:
-                print(cell, end = " ")
+            # if cell.has_mine():
+            #     print("*", end = " ")
+            # else:
+            print(cell, end = " ")
         print()
 
 
@@ -60,7 +69,40 @@ def place_mines(board, mines):
         board[y][x].set_mine() 
 # генератор, котрый создает список всех возможных позиций на доске
 
+def filter_cords(k):
+    return k[0]>=0 and k[1]>=0
 
+def open_space(board, x, y):
+        cell = board[y][x]
+        if cell.is_opened():
+            return
+        cell.open()
+        neighbours = [
+            (y-1, x-1), (y-1, x), (y-1, x+1),
+            (y, x-1),             (y, x+1),
+            (y+1, x-1), (y+1, x), (y+1,x+1)
+        ]
+        neighbours = list(filter(lambda k: k[0]>=0 and k[1]>=0, neighbours))
+
+        # cell.neigbouring_mines += 1
+        for (r, c) in neighbours:
+            try:
+                neighbour_cell = board[r][c]
+                if neighbour_cell.has_mine():
+                    cell.increment_neighbouring_mines()
+            except IndexError:
+                pass
+
+        if cell.get_neighbouring_mines_number() == 0:
+            for (r, c) in neighbours:
+                try:
+                    neighbour_cell = board[r][c]
+                    if not neighbour_cell.has_mine():
+                        open_space(board, c, r)
+                except IndexError:
+                    pass
+
+                
 def play(board):
     while True:
         display_board(board)
@@ -78,27 +120,16 @@ def play(board):
             print('Game over! You hit a mine.')
             break
         else:
-            cell.open()
-            neighbours = [
-                (y-1, x-1), (y-1, x), (y-1, x+1),
-                (y, x-1),             (y, x+1),
-                (y+1, x-1), (y+1, x), (y+1,x+1)
-            ]
-            # cell.neigbouring_mines += 1
-            for neighbour in neighbours:
-                (y, x) = neighbour
-                try:
-                    neighbour_cell = board[y][x]
-                    if neighbour_cell.has_mine():
-                        cell.increment_neighbouring_mines()
-                except IndexError:
-                    pass
+            open_space(board, x, y)
 
+        if all([board[r][c].has_mine() or board[r][c].is_opened() for r in range(len(board)) for c in range(len(board[0]))]):
+            print("You won!")
+            break
 def init_game(height, width, mines):
     board = create_board(height, width, mines)
     play(board)
 
-difficulty = input("Select your difficulty: 1- Easy 2- Normal 3- difficult 4- insane 5- hard as heck ").strip()
+difficulty = input("Select your difficulty: 1- Easy 2- Normal 3- difficult 4- insane 5- hard as heck ##- FUN value ").strip()
 
 match difficulty:
     case "1":
@@ -116,6 +147,9 @@ match difficulty:
     case "5":
         print("You selected hard as heck")
         init_game(15, 15, 75)
+    case "61":
+        print("You selected FUN value")
+        init_game(16, 16, 100)
     case _:
         print("Invalid selection")
         exit()
--- saturday-15-30/minesweeper/testing.py
+++ saturday-15-30/minesweeper/testing.py
@@ -1,6 +1,33 @@
-a, b = input().split(' ')
-print(a, b)
+# a, b = input().split(' ')
+# print(a, b)
 
-def testing():
-    return 5
+# def testing():
+#     return 5
 
+
+# 5! = 5*4*3*2*1
+# 5! = 5*4*3*2*1*1
+# 0! = 1
+
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+    
+def add(numbers):
+    if not numbers:
+        return 0
+    else:
+        return numbers[0] + add(numbers[1:])
+
+
+print(fact(5)) # 120
+print(fact(6))
+print(fact(1000))
+print(add([5,5,4,2])) # 16
+
+# [5,5,4,2]
+# 5 + 5 + 4 + 2 + 0
